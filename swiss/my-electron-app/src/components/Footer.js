5a36d8cc556d78f10ff4585a79bd1dcf7163b070
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2025-02-26 02:36:27 +0200

    add footer component with copyright notice

---
+++ swiss/my-electron-app/src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = ()=>{
+    return <>
+       <footer style={{ marginTop: "50px", fontSize: "18px",  }}>
+        © {new Date().getFullYear()} Sigmoid Softwares. All rights reserved.
+      </footer>
+    </>
+}
+export default Footer;
